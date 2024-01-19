56205447cd6065327afc259448de2b2d4ea3e967
Tasif Hossain Emon <tasifthe2002@gmail.com>
2024-01-19 23:16:14 +0600

    Initial commit

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Text scoreText;
+    public GameObject UI_MainMenu;
+
+    public GameObject UI_Setting;
+    public GameObject UI_HighScore;
+    public Slider slider;
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        UI_HighScore.SetActive(false);
+        UI_Setting.SetActive(false);
+
+        UI_MainMenu.SetActive(true);
+    }
+
+    public void HighScore()
+    {
+        scoreText.text = PlayerPrefs.GetInt("score").ToString();
+        UI_MainMenu.SetActive(false);
+
+        UI_HighScore.SetActive(true);
+    }
+
+    public void Setting()
+    {
+        UI_MainMenu.SetActive(false);
+        UI_Setting.SetActive(true);
+    }
+
+    public void SettingAudio()
+    {
+        AudioListener.volume = slider.value;
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.SetInt("score", 0);
+    }
+}
